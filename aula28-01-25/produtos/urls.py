2354bce917e52466985306ed75794a548657b500
Joel Santos <joeloliveira56@gmail.com>
2025-01-29 10:32:58 -0300

    resolvido

--- aula28-01-25/produtos/urls.py
+++ aula28-01-25/produtos/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import ProdutoViewSet
 
 router = DefaultRouter()
-router.register(r'produtos', ProdutoViewSet, basename='produto')
+router.register(r'produtos', ProdutoViewSet, basename='produtos')
 
 urlpatterns = router.urls
